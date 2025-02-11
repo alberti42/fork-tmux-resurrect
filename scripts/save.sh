86a1e50beb392077eb34faf743f7bc261df14de9
Andrea Alberti <a.alberti82@gmail.com>
2025-02-11 18:19:22 +0100

    Replaced soft link with copy operation

--- scripts/save.sh
+++ scripts/save.sh
@@ -245,8 +245,8 @@ save_all() {
 	dump_state   >> "$resurrect_file_path"
 	execute_hook "post-save-layout" "$resurrect_file_path"
 	if files_differ "$resurrect_file_path" "$last_resurrect_file"; then
-		ln -fs "$(basename "$resurrect_file_path")" "$last_resurrect_file"
-	else
+    cp -f "$resurrect_file_path" "$last_resurrect_file"
+  else
 		rm "$resurrect_file_path"
 	fi
 	if capture_pane_contents_option_on; then
